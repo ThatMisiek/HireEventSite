1386ca93667b3c8ba4013176b3e78a801be00fab
ThatMisiek <kowal147@vp.pl>
2023-10-19 22:25:25 +0200

    Add new files

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,133 @@
+import React, {
+	useState,
+	useEffect,
+	useReducer,
+	useContext,
+	useRef,
+} from "react";
+
+import { Link, useNavigate } from "react-router-dom";
+import AuthContext from "../../store/auth-context";
+import Input from "./Input";
+import Modal from "../UI/Modal";
+
+
+const emailReducer = (state, action) => {
+	if (action.type === "USER_INPUT") {
+		return { value: action.val, isValid: action.val.includes("@") };
+	}
+	if (action.type === "INPUT_BLUR") {
+		return { value: state.value, isValid: state.value.includes("@") };
+	}
+	return { value: "", isValid: false };
+};
+
+const passwordReducer = (state, action) => {
+	if (action.type === "USER_INPUT") {
+		return { value: action.val, isValid: action.val.trim().length > 6 };
+	}
+	if (action.type === "INPUT_BLUR") {
+		return { value: state.value, isValid: state.value.trim().length > 6 };
+	}
+	return { value: "", isValid: false };
+};
+
+const Login = (props, event) => {
+	const [formIsValid, setFormIsValid] = useState(false);
+	const navigate = useNavigate();
+
+	const [emailState, dispatchEmail] = useReducer(emailReducer, {
+		value: "",
+		isValid: null,
+	});
+	const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+		value: "",
+		isValid: null,
+	});
+
+	const authCtx = useContext(AuthContext);
+
+	const emailInputRef = useRef();
+	const passwordInputRef = useRef();
+
+	const { isValid: emailIsValid } = emailState;
+	const { isValid: passwordIsValid } = passwordState;
+
+	useEffect(() => {
+		const identifier = setTimeout(() => {
+			setFormIsValid(emailIsValid && passwordIsValid);
+		}, 500);
+
+		return () => {
+			clearTimeout(identifier);
+		};
+	}, [emailIsValid, passwordIsValid]);
+
+	const emailChangeHandler = (event) => {
+		dispatchEmail({ type: "USER_INPUT", val: event.target.value });
+	};
+
+	const passwordChangeHandler = (event) => {
+		dispatchPassword({ type: "USER_INPUT", val: event.target.value });
+	};
+
+	const validateEmailHandler = () => {
+		dispatchEmail({ type: "INPUT_BLUR" });
+	};
+
+	const validatePasswordHandler = () => {
+		dispatchPassword({ type: "INPUT_BLUR" });
+	};
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+		if (formIsValid) {
+			authCtx.onLogin(emailState.value, passwordState.value);
+		} else if (!emailIsValid) {
+			emailInputRef.current.focus();
+		} else {
+			passwordInputRef.current.focus();
+		}
+	};
+
+	return (
+		<>
+			<Modal onClose={() => navigate("../")}>
+				<form onSubmit={submitHandler}>
+					<Input
+						ref={emailInputRef}
+						id='email'
+						label='E-Mail'
+						type='email'
+						isValid={emailIsValid}
+						value={emailState.value}
+						onChange={emailChangeHandler}
+						onBlur={validateEmailHandler}
+					/>
+					<Input
+						ref={passwordInputRef}
+						id='password'
+						label='Password'
+						type='password'
+						isValid={passwordIsValid}
+						value={passwordState.value}
+						onChange={passwordChangeHandler}
+						onBlur={validatePasswordHandler}
+					/>
+					<div className='form-actions'>
+						<>
+							<Link to='../' className='button-text'>
+								Cancel
+							</Link>
+							<button type='submit' className='button'>
+								Login
+							</button>
+						</>
+					</div>
+				</form>
+			</Modal>
+		</>
+	);
+};
+
+export default Login;
